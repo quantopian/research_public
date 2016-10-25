ed5a4699dfa8d3ff91ea150bb4b20c94abd38369
Andrew Hamlet <ash487@stern.nyu.edu>
2016-10-24 21:35:30 -0400

    Update pairs.py
    Greetings Quantopian Community,
    
    I was at the NYC Event on Pairs Trading, and the current example algorithm is deprecated, such that one cannot deploy it in live trading. With this fix, users can now deploy the algorithm in live trading.

--- lectures/pairs_trading/pairs.py
+++ lectures/pairs_trading/pairs.py
@@ -9,25 +9,24 @@ def initialize(context):
     # Quantopian backtester specific variables
     set_slippage(slippage.FixedSlippage(spread=0))
     set_commission(commission.PerTrade(cost=1))
-    set_symbol_lookup_date('2014-01-01')
-    
-    context.stock_pairs = [(symbol('ABGB'), symbol('FSLR')),
-                           (symbol('CSUN'), symbol('ASTI'))]
+    set_symbol_lookup_date('2010-01-01')
+
+    context.stock_pairs = [(symbol(' '), symbol(' '))]
     # set_benchmark(context.y)
-    
+
     context.num_pairs = len(context.stock_pairs)
     # strategy specific variables
     context.lookback = 20 # used for regression
     context.z_window = 20 # used for zscore calculation, must be <= lookback
-    
+
     context.spread = np.ndarray((context.num_pairs, 0))
     # context.hedgeRatioTS = np.ndarray((context.num_pairs, 0))
     context.inLong = [False] * context.num_pairs
     context.inShort = [False] * context.num_pairs
-    
+
     # Only do work 30 minutes before close
     schedule_function(func=check_pair_status, date_rule=date_rules.every_day(), time_rule=time_rules.market_close(minutes=30))
-    
+
 # Will be called on every trade event for the securities you specify. 
 def handle_data(context, data):
     # Our work is now scheduled in check_pair_status
@@ -36,17 +35,21 @@ def handle_data(context, data):
 def check_pair_status(context, data):
     if get_open_orders():
         return
-    
-    prices = history(35, '1d', 'price').iloc[-context.lookback::]
-    
+
     new_spreads = np.ndarray((context.num_pairs, 1))
-    
+
     for i in range(context.num_pairs):
 
         (stock_y, stock_x) = context.stock_pairs[i]
 
-        Y = prices[stock_y]
-        X = prices[stock_x]
+
+       ***THE CHANGE***
+       ######################################################
+
+        Y = data.history(stock_y, 'price', 35, '1d').iloc[-context.lookback::]
+        X = data.history(stock_x, 'price', 35, '1d').iloc[-context.lookback::]
+
+       ######################################################
 
         try:
             hedge = hedge_ratio(Y, X, add_const=True)      
@@ -55,7 +58,7 @@ def check_pair_status(context, data):
             return
 
         # context.hedgeRatioTS = np.append(context.hedgeRatioTS, hedge)
-        
+
         new_spreads[i, :] = Y[-1] - hedge * X[-1]
 
         if context.spread.shape[1] > context.z_window:
@@ -104,7 +107,7 @@ def check_pair_status(context, data):
                 order_target_percent( stock_y, y_target_pct * (1.0/context.num_pairs) / float(context.num_pairs) )
                 order_target_percent( stock_x, x_target_pct * (1.0/context.num_pairs) / float(context.num_pairs) )
                 record(Y_pct=y_target_pct, X_pct=x_target_pct)
-        
+
     context.spread = np.hstack([context.spread, new_spreads])
 
 def hedge_ratio(Y, X, add_const=True):
@@ -114,7 +117,7 @@ def hedge_ratio(Y, X, add_const=True):
         return model.params[1]
     model = sm.OLS(Y, X).fit()
     return model.params.values
-    
+
 def computeHoldingsPct(yShares, xShares, yPrice, xPrice):
     yDol = yShares * yPrice
     xDol = xShares * xPrice
